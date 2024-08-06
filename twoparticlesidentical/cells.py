fea55bb932552e8704a8f120a11338a5c9ecc224
Oliver Brown <oliverbrown314@gmail.com>
2024-08-06 16:57:45 +0100

    Changes to be committed: 	new file:   twoparticlesidentical/__pycache__/cells.cpython-311.pyc 	new file:   twoparticlesidentical/__pycache__/domains.cpython-311.pyc 	modified:   twoparticlesidentical/cells.py 	new file:   twoparticlesidentical/configs.py
    Note: The ConfigurationSpace class now contains methods for gluing cells together. This is a work in progress.

--- twoparticlesidentical/cells.py
+++ twoparticlesidentical/cells.py
@@ -17,19 +17,22 @@ class SquareCell:
             length: The side length of the square domain.
         """
 
+        # Creae a grid graph of the given dimensions.
         self.N = length
         self.G = nx.grid_graph(dim=(length,length))
 
+        # Find the corners and edges of the domain.
         self.corners = self.find_corners()
         self.edges = self.find_edges()
 
-        #self.find_edges()
-        #self.removed_nodes = []
-        #self.non_elim_coords = []
-        #self.non_elim_nodes = []
-        #self.num_non_elim = None
-        #self.indices = None
-        #self.eigenstates = None
+        # Initialize the remaining attributes.
+        self.removed_nodes = []
+        self.non_elim_coords = []
+        self.non_elim_nodes = []
+        self.num_non_elim = None
+        self.indices = None
+        self.eigenstates = None
+
         return None
 
     def find_corners(self):
@@ -71,48 +74,172 @@ class SquareCell:
         nodes_list = list(self.G.nodes())
         
         # Find the nodes on the first y-axis
-        y0_nodes = []
-        for x in range(1,N-1):
-            y0_nodes.append(nodes_list[x])
-            pass
-        
-        # Find the nodes on the first x-axis
         x0_nodes = []
-        for x in range(N,(N**2)-N,N):
+        for x in range(1,N-1):
             x0_nodes.append(nodes_list[x])
             pass
         
-        # Find the nodes on the opposite y-axis
-        y1_nodes = []
-        for x in range((N**2)-N+1,(N**2)-1):
-            y1_nodes.append(nodes_list[x])
+        # Find the nodes on the first x-axis
+        y0_nodes = []
+        for x in range(N,(N**2)-N,N):
+            y0_nodes.append(nodes_list[x])
             pass
         
-        # Find the nodes on the opposite x-axis
+        # Find the nodes on the opposite y-axis
         x1_nodes = []
-        for x in range(2*N-1,N**2-1,N):
+        for x in range((N**2)-N+1,(N**2)-1):
             x1_nodes.append(nodes_list[x])
             pass
         
-        # Find the nodes along the diagonal x=y
-        # diag_nodes = []
-        # for x in range(N+1,(N**2 - 1), N + 1):
-        #     diag_nodes.append(nodes_list[x])
-        #     pass
+        # Find the nodes on the opposite x-axis
+        y1_nodes = []
+        for x in range(2*N-1,N**2-1,N):
+            y1_nodes.append(nodes_list[x])
+            pass
 
-        #x0 = Edge(self,x0_nodes)
-        #y0 = Edge(self,y0_nodes)
-        #x1 = Edge(self,x1_nodes)
-        #y1 = Edge(self,y1_nodes)
-        #diag = Edge(self,diag_nodes)
+        # Create edge objects
+        x0 = Edge(self,x0_nodes)
+        y0 = Edge(self,y0_nodes)
+        x1 = Edge(self,x1_nodes)
+        y1 = Edge(self,y1_nodes)
         
-        edges.append(x0_nodes)
-        edges.append(y0_nodes)
-        edges.append(x1_nodes)
-        edges.append(y1_nodes)
-        #edges.append(diag)
+        # Append the edges to a list
+        edges = []
+        edges.append(x0)
+        edges.append(y0)
+        edges.append(x1)
+        edges.append(y1)
+        
+        # Assign the edges as class variables
+        self.x0 = x0
+        self.y0 = y0
+        self.x1 = x1
+        self.y1 = y1
         
         return edges
+
+    def lapl(self):
+        """
+        Generate the Laplacian for the square domain.
+
+        Args:
+            self: The SquareCell object.
+        """
+        
+        N = self.N
+        G = self.G
+        corners = self.corners
+
+        # Calculate Laplacian for Graph G
+        L = nx.laplacian_matrix(G)
+        
+        # Get list of nodes
+        nodes_list = list(G.nodes())
+        
+        removed_nodes = []
+        # Delete corners from the Laplacian matrix
+        
+        self.removed_nodes = removed_nodes
+        
+        self.L = L
+        
+        return None
+
+class TriangleCell(SquareCell):
+
+    def __init__(self, length):
+        """
+        Initialize the TriangleCell object with the given parameters.
+
+        Args:
+            self: The TriangleCell object.
+            length: The side length of the square domain.
+        """
+
+        # Create a grid graph of the given dimensions.
+
+        super().__init__(length)
+
+        # Find the diagonal of the domain.
+        diag = self.find_diag()
+        self.edges.append(diag)
+
+        # Delete nodes beyond the diagonal from the domain in order to form a triangular cell.
+        self.G = self.form_triangle()
+
+        return None
+
+    def find_diag(self):
+        """
+        Find the nodes in the graph corresponding to the diagonal of the domain.
+
+        Args:
+            self: The TriangleCell object.
+        
+        Returns:
+            diag: The nodes corresponding to the diagonal of the domain.
+        """
+
+        # Initialize variables.
+        N = self.N
+        nodes_list = list(self.G.nodes())
+
+        # Find the nodes along the diagonal x=y.
+        diag_nodes = []
+        for x in range(N+1,(N**2 - 1), N + 1):
+            diag_nodes.append(nodes_list[x])
+            pass
+
+        # Create an edge object for the diagonal.
+        diag = Edge(self,diag_nodes)
+
+        # Create a class variable for the diagonal.
+        self.diag = diag
+
+        return diag
+
+    def form_triangle(self):
+        """
+        Form a triangular domain from the square domain.
+
+        Args:
+            self: The TriangleCell object.
+        """
+
+        N = self.N
+        G = self.G
+
+        # Find the nodes in the graph corresponding to the corners of the domain.
+        corners = self.corners
+
+        # Calculate Laplacian for Graph G
+        L = nx.laplacian_matrix(G)
+
+        # Get list of nodes
+        nodes_list = list(G.nodes())
+
+        # Create list of indices to drop from the Laplacian matrix.
+        drop_indices = []
+        for i in range (N):
+            drop_indices += [d for d in range(i*N+i+1,(i+1)*N)]
+            pass
+
+        # Identify the nodes to drop from the graph.
+        drop_nodes = [nodes_list[i] for i in drop_indices]
+
+        # Drop excess nodes from the grid graph.
+        G.remove_nodes_from(drop_nodes)
+
+        # Delete corners and extra nodes from the objects's data.
+        self.corners.remove(nodes_list[N-1])
+
+        self.edges.remove(self.x0)
+        self.x0 = None
+        self.edges.remove(self.y1)
+        self.y1 = None
+        
+
+        return G
 
 class Domain:
     # A class for discretisation of square domains, which can be glued together
@@ -467,11 +594,54 @@ class Domain:
         return None
     
 class Edge:
-    # A class for the edge of a two-particle domain
+    """
+    A class containing data about the edges of a two-particle domain.
+    """
 
     def __init__(self, domain, edge_nodes):
+        """
+        Initialize the Edge object with the given parameters.
+
+        Args:
+            self: The Edge object.
+            domain: The Domain object to which the edge belongs.
+            edge_nodes: The nodes corresponding to the edge of the domain.
+        """
+
         self.domain = domain
         self.edge = edge_nodes
+
+        return None
+
+    def find_edge_indices_in_configuration_space(self, cfgspace):
+        """
+        Find the indices of the nodes corresponding to the edge in the configuration space.
+
+        Args:
+            self: The Edge object.
+            cfgspace: The configuration space of the system.
+        """
+
+        cells = cfgspace.cells
+
+        edge = self.edge
+        domain = self.domain
+
+        nodes_list = list(domain.G.nodes())
+        edge_coords = [nodes_list.index(x) for x in edge]
+        
+        i = cells.index(domain)
+        for cell in cells:
+            if cells.index(cell) < i:
+                t = np.array(edge_coords)
+                t += cell.G.number_of_nodes()
+                edge_coords = t.tolist()
+                pass
+            else:
+                pass
+            pass
+        
+        self.edge_indices = edge_coords
 
         return None
 
@@ -491,135 +661,18 @@ def lapl_spectrum(matrix,h,dps):
 if __name__=="__main__":
     # Main
     
-    N = 10 # The side length of the square
+    N = 5 # The side length of the square
     
     # Scaling factor
     h = (np.pi)/(N-1)
     
-    D = SquareCell(N)
-    G = D.G
-    print(G)
-    
-    #D.lapl()
-    #D.apply_neumann(D.x0)
-    #D.apply_neumann(D.y0)
-    #D.apply_neumann(D.x1)
-    #D.apply_neumann(D.y1)
-    #print(D.L)
-    #print(D.L.toarray())
-    
-    
-    # Calculate the spectrum of the Laplacian
-    
-    #spectrum = lapl_spectrum(D.L,h,2)
-    #print(spectrum)
+    D = TriangleCell(N)
+    print(D.G)
+    print(D.G.nodes())
+    print(D.corners)
+    for e in D.edges:
+        print(e.edge)
+        pass
 
-
-
-
-
-
-
-
-
-
-"""
-def dirichlet_lapl(G):
-    # Calculate the Laplacian matrix with Dirichlet boundary conditions for a grid G
-    
-    # Find the size of the graph
-    N = int(np.sqrt(nx.number_of_nodes(G)))
-    
-    # Calculate Laplacian for Graph G
-    L = nx.laplacian_matrix(G)
-    
-    # Generate list of boundary "end nodes"
-    nodes_list = list(G.nodes())
-    
-    # Generate a list of the indices of nodes which are on the boundary of the square
-    end_indices = []
-    for x in range(0,N):
-        end_indices.append(x)
-    for x in range(N,(N**2)-N,N):
-        end_indices.append(x)
-    for x in range(2*N-1,N**2-1,N):
-        end_indices.append(x)
-    for x in range((N**2)-N,N**2):
-        end_indices.append(x)
-    
-    end_indices.sort()
-    end_nodes = []
-    for i in end_indices:
-        end_nodes.append(nodes_list[i])
-    
-    #end_nodes = [x for x in G.nodes() if G.degree(x)==2]
-    
-    # Modify Laplacian for Dirichlet boundary conditions at each "end node"
-    c = 0
-    for n in end_nodes:
-        i = nodes_list.index(n)
-        #L[i,:] = 0
-        #L[:,i] = 0
-        c = N**2 - L.shape[0]
-        i -= c
-        L = sp.sparse.vstack([L[:i,:],L[i+1:,:]],format='csr')
-        L = sp.sparse.hstack([L[:,:i],L[:,i+1:]],format='csr')
-        #c += 1
-        
-    c = 0   
-    
-    L.eliminate_zeros()
-    
-    return L
-"""
-
-"""
-def neumann_lapl(G):
-    # Calculate the Laplacian matrix with Neumann boundary conditions for a grid G
-    
-    # Find the size of the graph
-    N = int(np.sqrt(nx.number_of_nodes(G)))
-    
-    # Calculate Laplacian for Graph G
-    L = nx.laplacian_matrix(G)
-    
-    # Generate list of boundary "end nodes"
-    nodes_list = list(G.nodes())
-    
-    # Generate a list of the indices of nodes which are on the boundary of the square
-    end_indices = []
-    for x in range(0,N):
-        end_indices.append(x)
-    for x in range(N,(N**2)-N,N):
-        end_indices.append(x)
-    for x in range(2*N-1,N**2-1,N):
-        end_indices.append(x)
-    for x in range((N**2)-N,N**2):
-        end_indices.append(x)
-    
-    end_indices.sort()
-    end_nodes = []
-    for i in end_indices:
-        end_nodes.append(nodes_list[i])
-    
-    #end_nodes = [x for x in G.nodes() if G.degree(x)==2]
-    
-    # Modify Laplacian for Neumann boundary conditions at each "end node"
-    # Begin by deleting end nodes from the matrix
-    c = 0
-    for n in end_nodes:
-        i = nodes_list.index(n)
-        #L[i,:] = 0
-        #L[:,i] = 0
-        i -= c
-        L = sp.sparse.vstack([L[:i,:],L[i+1:,:]],format='csr')
-        L = sp.sparse.hstack([L[:,:i],L[:,i+1:]],format='csr')
-        c += 1
-    c = 0
-    
-    # Modifies rows of the L matrix according to the central difference approximation
-    
-    L.eliminate_zeros()
-    
-    return L
-"""
+    D.lapl()
+    print(D.L.toarray())
